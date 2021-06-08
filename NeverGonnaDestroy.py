ecb70078bd5daf1ff3881e9e4e92ed03b5ade867
aray03 <aidan@andersenweb.com>
2021-06-08 14:04:15 -0600

    Initial Commiit

---
+++ NeverGonnaDestroy.py
@@ -0,0 +1,24 @@
+import os
+stall=input("\n\n\n\n\n\n\n\n<><><> Press Enter To Begin <><><>")
+amount=input("How many copies do we need to remove?: ")
+
+count = 0
+newNum = 0
+amount=int(amount)
+for num in range(amount):
+
+    try:
+        os.remove('NeverGonna' +str(num+1) + '.mp4')
+        count = count+1
+    except FileNotFoundError:
+       count = count
+
+    #The print statement to make sure we are not repeating %s
+    if int((num/amount)*10000)/100 > newNum:
+        print(str(int((num/amount)*10000)/100) + "%")
+        newNum = int((num/amount)*10000)/100
+
+os.system('cls' if os.name == 'nt' else 'clear')
+print("100%")
+print("Deleted", count, "Files")
+print("The Work Has Been Finished!")
